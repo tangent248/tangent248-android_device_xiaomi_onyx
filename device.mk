b5e479284533b8a50f6c86d4fecc6974bd181042
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-09 12:52:37 +0300

    onyx: rootdir: Don't configure zram in QCOM's init post boot script
    * Configure it from fstab.zram instead
    
    Change-Id: Ie84d0674a643e637d5ef894399d76ede25d00eeb

--- device.mk
+++ device.mk
@@ -195,7 +195,8 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_PACKAGES += \
     charger_fw_fstab.qti \
-    fstab.qcom
+    fstab.qcom \
+    fstab.zram
 
 PRODUCT_PACKAGES += \
     fw_dir.ueventd.qcom.rc \
@@ -222,7 +223,8 @@ PRODUCT_PACKAGES += \
     init.qcrild.sh
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram
 
 # IR
 PRODUCT_PACKAGES += \
