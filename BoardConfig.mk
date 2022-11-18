5f42bda64e9513fc93403fac8a376b21fc26316d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-18 08:28:36 +0545

    onyx: Disable kpti
    * As per qualcomm - SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not required.
    * It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Iddd31f7defe570b7752a56f612df0131b46df08e
    Signed-off-by: Zarathos30 <vale.rigni@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,8 +85,9 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     swinfo.fingerprint=$(LINEAGE_VERSION) \
     mtdoops.fingerprint=$(LINEAGE_VERSION) \
-    bootmonitor.fingerprint=$(LINEAGE_VERSION)
-
+    bootmonitor.fingerprint=$(LINEAGE_VERSION) \
+    kpti=off
+	
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
