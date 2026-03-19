35582c71bcfe4ea7d085fc5fc0b009742a84dcc0
Ethan Chen <intervigil@gmail.com>
2026-03-19 15:23:42 +0000

    onyx: Remove unused init.qcom.class_core script * This script is a no-op for the sun platform.
    Change-Id: I71fef49d4a81f3dbeffc89d87dcf63fa56579bf0
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -241,7 +241,6 @@ PRODUCT_PACKAGES += \
     init.kernel.post_boot-tuna.sh \
     init.kernel.post_boot-tuna_default_2_3_2_1.sh \
     init.kernel.post_boot.sh \
-    init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
