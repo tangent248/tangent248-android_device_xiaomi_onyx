4fe4b814a75d00a5bb49769043f0b0a7baa3de0d
Rve27 <rve27github@gmail.com>
2026-05-12 08:48:20 +0000

    onyx: Add MLBB and MCGG to sched_lib_name
    Signed-off-by: Rve27 <rve27github@gmail.com>
    Signed-off-by: romgharti <romgharti@gmail.com>

--- rootdir/bin/init.kernel.post_boot-tuna_default_2_3_2_1.sh
+++ rootdir/bin/init.kernel.post_boot-tuna_default_2_3_2_1.sh
@@ -88,7 +88,7 @@ if [ -d /proc/sys/walt ]; then
 	echo 0x60 > /proc/sys/walt/sched_sbt_pause_cpus
 
 	# Report max frequency to unity tasks.
-	echo "UnityMain,libunity.so,libil2cpp.so,com.miHoYo.,com.pubg.imobile,com.pubg.,com.activision." > /proc/sys/walt/sched_lib_name
+	echo "UnityMain,libunity.so,libil2cpp.so,com.miHoYo.,com.pubg.imobile,com.pubg.,com.activision.,com.mobilechess.gp,com.mobile.legends" > /proc/sys/walt/sched_lib_name
 	echo 255 > /proc/sys/walt/sched_lib_mask_force
 
 	# Setting b.L scheduler parameters
