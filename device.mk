8b90b8a927b39afda44085d5fd96595d4cc63d44
EmanuelCN <EmanuelCN.noreply.github.com>
2026-06-26 23:56:21 +0530

    Onyx : Disable ART and dex2oat debugging
    (cherry picked from commit bcabd1ad59f50b90cd5b225786f7c40b6ead0c3c)

--- device.mk
+++ device.mk
@@ -22,6 +22,15 @@ $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk
 $(call soong_config_set,rfs,mpss_firmware_symlink_target,modem_firmware)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# ART
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+USE_DEX2OAT_DEBUG := false
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
